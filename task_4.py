32ccbd8e6c97347adc33fef5c34752ac8d458dc9
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2023-01-04 19:23:28 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,2 +1,11 @@
 def reversed_words():
-    return []
+    reversed_words = []
+    words = set()
+    for line in open("words.txt", encoding="utf-8"):
+        words.add(line.strip())
+    for word in words:
+       if word[::-1] in words:
+           pair = word, word[::-1]
+           reversed_words.append(pair)
+    reversed_words.sort()
+    return reversed_words
